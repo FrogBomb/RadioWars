e4fbc84a5911e593866cd94a51f127c5d30dec29
FrogBomb <TLBlanchet@gmail.com>
2016-05-31 11:33:06 -0600

    datafiles + backend mod
    Added default datafiles and modified the back end to read those files to
    prepare to work on making something render on the front end.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 	var PORT = 3000;
 	var BASE_HTML_FILE = './dist/index.html'
 	
+	var fs = require('fs');
+	
 	var express = require('express');
 	var bodyParser = require('body-parser');
 	var cookieParser = require('cookie-parser');
@@ -25,8 +27,34 @@
 	//All availible rooms
 	var ROOMS = [];
 	
+	var MAPS = {loaded: false};
+	
+	var DEFAULT_MAP_NAME = "alpha";
+	
+	//Loading the maps here, which then generates the default rooms.
+	loadMaps();
+	
+	function loadMaps(){
+		fs.readfile('./maps.json', function(err, data){
+			if(err){return console.log(err);}
+			MAPS = JSON.parse(data);
+			MAPS.loaded = true;
+
+			makeRooms();
+		});
+	}
+	
+	function makeRooms(){
+		//Clears the existing rooms
+		ROOMS.length = 0;
+		//Generating default rooms
+		ROOMS.push(Room('noob room', MAPS['alpha']));
+		ROOMS.push(Room('kinda good room', MAPS['alpha']));
+		ROOMS.push(Room('pro room', MAPS['alpha']));
+	}
+	
 	//Constructor for a new RadioState
-	function RadioGroupState(state, time){
+	function RadioGroupState(time, state){
 		//integer indicating the current value of each radio button group, and the time when it was last clicked.
 		if(!state){
 			state = 0;
@@ -46,41 +74,45 @@
 		}
 	});
 	
-	//Constructor for a new GameState (Reference for what the status of the game is)
+	//Constructor for a new GameState (Reference for what the status of the game is). numRadios arguement is optional.
 	function GameState(numRadios){
 		//array of radioGroupStates, indicating the current value of each radio button group.
 		this.radios = [];
 		for(var i = 0; i<numRadios; i++){
 			this.radios.push(new RadioGroupState());
 		}
-		
-		
 	}
 
 	GameState.prototype = Object.create({
 		updateRadio: function(radio, i){
-			this.radios[i].update(radio.time, radio.state);
+			if(this.radios){
+				this.radios[i].update(radio.time, radio.state);
+			}
+			else{
+				this.radio[i] = new RadioGroupState(radio.time, radio.state);
+			}
 		}
 	});
 	
+	function readMapFileCallback(err, data){
+		if(err){return console.log(err);}
+		this.mapData = JSON.parse(data);
+		this.mapFileRead = true;
+	}
 	
 	//Constructor for a new room (Holds the game and manages players)
-	function Room(name, numPlayers, numRadios, pageInfoRef){
+	function Room(name, mapInfoRef){
 		this.name = name;//Name of the room
-		this.pageInfoRef = pageInfoRef;//JSON file reference for the front end to generate the game room. 
-		this.numPlayers = numPlayers;
-		
+		this.mapInfoRef = mapInfoRef;//file reference to generate the game room.
+		this.mapData; // Will hold the map data object from mapInfoRef
+		this.mapFileRead = false;
+		fs.readfile(mapInfoRef, readMapFileCallback.bind(this))
 		//Server side game state.
-		this.gameState = new GameState(numRadios);
+		this.gameState = new GameState();
 		//array of current mouse positions (pos), velocities (vel), and update timestamps (time).
 		this.mouses = [];
 		//array of previous mouse positions (pos), velocities (vel), and update timestamps (time).
 		this.prevMouses = [];
-		
-		for(var i = 0; i<numPlayers; i++){
-			this.mouses.push({pos:[0, 0], vel: [0, 0], time: 0});
-			this.prevMouses.push({pos:[0, 0], vel: [0, 0], time: 0});
-		}
 	}
 	
 	
@@ -90,8 +122,28 @@
 				this.prevMouses[index] = this.mouses[index];
 				this.mouses[index] = {pos: newP, vel: newV, time: time}; 
 			}
+		},
+		/*
+		Updates the room to have a new player and
+		returns the roomIndex to be able to interact with the room.
+		*/
+		addPlayer: function(){
+			
+			this.mouses.push({pos:[-200, -200], vel: [0, 0], time: -1});
+			this.prevMouses.push({pos:[-200, -200], vel: [0, 0], time: -1});
+			
+			return this.mouses.length - 1;
+			
+		},
+		//Returns radio team of the player with the passed roomIndex
+		getRadioTeam: function(roomIndex){
+			if(this.mapFileRead){
+				return roomIndex%this.mapInfo.numTeams;
+			}
+			return -1;//Map file not yet read
 		}
 	});
+	
 	//Gets the player room index of the request
 	function getPIndexOf(req){
 		return req.session.roomIndex;
@@ -106,7 +158,7 @@
 	}
 	//Gets the mouse data of the request to put directly into room.updateMouse
 	function getMouseDataOf(req){
-		return [req.session.pos, req.session.vel, req.session.time, getPIndexOf(req)]
+		return [req.session.pos, req.session.vel, req.session.time, getPIndexOf(req)];
 	}
 	
 	//GET ROOT: send the base html page. 
@@ -119,16 +171,17 @@
 		req.session.username = req.body.username;
 		res.send("Logged in as: " + req.session.username);
 	});
-	
 	//GET /gameroom: send JSON file for the gameroom.
 	app.get('/gameroom', function(req, res){
-		res.sendfile(getRoomOf(req).pageInfoRef);
+		res.sendfile(getRoomOf(req).mapInfoRef);
 	});
 	
 	//POST /gameroom: assign the room number of the player to the requested room number.
 	app.post('/gameroom', function(req, res){
 		req.session.roomNumber = req.body.roomNumber;
-		res.send("success");
+		var room = getRoomOf(req);
+		req.session.roomIndex = room.addPlayer();
+		res.send(JSON.stringify({radioTeam: room.getRadioTeam(res.session.roomIndex)}));
 	});
 	
 	//POST /mouses: sends info about the current player's mouse.
@@ -156,6 +209,7 @@
 		var room = getRoomOf(req);
 		res.send(JSON.stringify(room.gameState.radios));
 	});
+	
 	
 	
 	app.use(express.static('public'));
